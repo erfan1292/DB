25239ef7e1da0380b8c9999c709ff1438da930dc
erfan1292 <mansori_erfan@yahoo.com>
2016-05-10 00:24:01 +0430

    Update links.lua

--- plugins/links.lua
+++ plugins/links.lua
@@ -1,11 +1,19 @@
-local function run(msg)
-    if not is_momod(msg) then
-        chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'No links ... Kick Spammer'
+local function run(msg, matches)
+    if not permissions(msg.from.id, msg.to.id, "settings") then
+        local hash = 'links:'..msg.to.id
+        if redis:get(hash) then
+            delete_msg(msg.id, ok_cb, false)
+            send_report(msg)
+        end
     end
 end
 
-return {patterns = {
-    "https://telegram.me/joinchat/",
-    "http://telegram.me/joinchat/",
-    }, run = run}
+return {
+patterns = {
+    -- Agrega mas links si es necesario
+    "[Hh][Tt][Tt][Pp][Ss][:][/][/]",
+    "[Hh][Tt][Tt][Pp][:][/][/]",
+    "[Ww][Ww][Ww][.]",
+    "[.][Cc][Oo][Mm]",
+    "https://[%w-_%.%?%.:/%+=&]+%"
+}, run = run}
